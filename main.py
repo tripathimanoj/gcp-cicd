47011755d201cc21e2fda099c84a9e69a8419879
Sarath kumar <03sarath@gmail.com>
2024-04-22 14:12:47 +0530

    chnages the app use case

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-import datetime
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [
-        datetime.datetime(2018, 1, 1, 10, 0, 0),
-        datetime.datetime(2018, 1, 2, 10, 30, 0),
-        datetime.datetime(2018, 1, 3, 11, 0, 0),
-    ]
+@app.route('/generate', methods=['POST'])
+def generate_output():
+    user_input = request.form['userInput']
+    # Here you would call your Generative AI model with the user input
+    # For simplicity, let's just echo the user input as output
+    return render_template('index.html', output=user_input)
 
-    return render_template("index.html", times=dummy_times)
-
-
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
+if __name__ == '__main__':
+    #app.run(debug=True)
     app.run(host="127.0.0.1", port=8080, debug=True)
