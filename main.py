5c4d1f8447362d77b5ef93d811b68793e74d1513
Sarath Kumar <03sarath@gmail.com>
2024-08-08 20:56:10 +0530

    modified main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def generate_output():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run() #, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
